c2f623d8075c64f4b002ef5d05fea2033fa85321
patilSmruti18 <smruti18102002@gmail.com>
2026-09-22 23:34:56 +0530

    demo2 java 8 question

---
+++ src/java8practiceQuestion/Demo2.java
@@ -0,0 +1,24 @@
+package java8practiceQuestion;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+
+        List<String> languages =
+                Arrays.asList("Java", "Python", "Java", "C++", "Python");
+
+        List<String> result =
+                languages.stream()
+                        .distinct()
+                        .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
+
+
