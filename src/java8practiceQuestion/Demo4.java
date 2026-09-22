8ff59e89b214717edb7d75858aebff34f005c68d
patilSmruti18 <smruti18102002@gmail.com>
2026-09-22 23:40:49 +0530

    demo4 java 8 questions

---
+++ src/java8practiceQuestion/Demo4.java
@@ -0,0 +1,33 @@
+package java8practiceQuestion;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+public class Demo4{
+
+
+
+    public static void main(String[] args) {
+
+        List<String> items =
+                Arrays.asList(
+                        "Pen",
+                        "Pencil",
+                        "Pen",
+                        "Eraser",
+                        "Pencil",
+                        "Pen"
+                );
+
+        Map<String, Long> result =
+                items.stream()
+                        .collect(Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting()
+                        ));
+
+        System.out.println(result);
+    }
+}
+
