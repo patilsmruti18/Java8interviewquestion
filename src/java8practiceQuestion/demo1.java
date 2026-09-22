bef4b3d54981d9f56faa583e20b035a5926f05fa
patilSmruti18 <smruti18102002@gmail.com>
2026-09-22 23:22:43 +0530

    update demo1 Java 8 questions

--- src/java8practiceQuestion/demo1.java
+++ src/java8practiceQuestion/demo1.java
@@ -1,4 +1,19 @@
 package java8practiceQuestion;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+    public class demo1 {
 
-public class demo1 {
-}
+
+        public static void main(String[] args) {
+
+            List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30, 35);
+
+            Map<Boolean, List<Integer>> result = numbers.stream()
+                    .collect(Collectors.partitioningBy(n -> n % 2 == 0));
+
+            System.out.println("Even Numbers: " + result.get(true));
+            System.out.println("Odd Numbers: " + result.get(false));
+        }
+    }
