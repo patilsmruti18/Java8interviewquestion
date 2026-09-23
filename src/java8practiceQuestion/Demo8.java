447d599944d61ccdfb0eb698d907d2343aef3153
patilSmruti18 <smruti18102002@gmail.com>
2026-09-23 09:47:38 +0530

    demo8 java questions

---
+++ src/java8practiceQuestion/Demo8.java
@@ -0,0 +1,28 @@
+package java8practiceQuestion;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Demo8 {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers =
+                Arrays.asList(45, 12, 89, 34, 67, 10);
+
+        int maximum =
+                numbers.stream()
+                        .max(Comparator.naturalOrder())
+                        .get();
+
+        int minimum =
+                numbers.stream()
+                        .min(Comparator.naturalOrder())
+                        .get();
+
+        System.out.println("Maximum: " + maximum);
+        System.out.println("Minimum: " + minimum);
+    }
+}
+
+
